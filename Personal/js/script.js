0fbc02714c0c2b67eff00f6e19cdb4cf58c31b6d
joannamok <joannamok90@gmail.com>
2013-05-01 21:22:02 -0500

    added comments

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,24 +1,25 @@
 //Joanna Mokhtarezadeh  5-1-2013
 //Personal
-//How many calories do I eat in a day
+//How many calories do I eat in a day based the same breakfast and snack everyday
 
 bcals = function(coffee,cearal){	//function for total calories for a cup of coffee and a bowl of mini wheats
 	var bfastTotal = coffee + cearal;
 	return bfastTotal;
 }
 
-scals = function(apple,orange){
+scals = function(apple,orange){ //function for a daily snack of 1 apple and 1 orange
 	var snackTotal = apple + orange;
 	return snackTotal;
 }  
 
-var b = bcals(100, 270);
+var b = bcals(100, 270); //calories cup of coffee and bowl of frosted miniwheats
 
-var s = scals(55,65);
+var s = scals(55,65); // calories of apple and orange
 
-var lunchCals = prompt("How many calories did you have for lunch?");
-var dinnerCals = prompt("How many calories did you have for dinner?");
-var totalDay = Number(lunchCals) + Number(dinnerCals) + b + s;
+var lunchCals = prompt("How many calories did you have for lunch?"); // User prompt for amount of lunch calories
+var dinnerCals = prompt("How many calories did you have for dinner?"); // User promt for amount of dinner calories 
+
+var totalDay = Number(lunchCals) + Number(dinnerCals) + b + s; // total calories for entire day,including breakfst,lunch,dinner and a snack.
 
 console.log(totalDay);
  
