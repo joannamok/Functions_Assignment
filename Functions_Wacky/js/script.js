bafeedb8af30872496a6783b2fc153bb6fb72951
joannamok <joannamok90@gmail.com>
2013-05-02 18:31:20 -0500

    added variable for total money raised

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -15,15 +15,16 @@ var bakedPack = function(basket,brownies){
 var k = kidsPack(10,5);
 var kidsPackSold = prompt("How many Children's Book Totebags did this student sell? ");
 var kidsPackTotal = k * Number(kidsPackSold);
-//var couponBook = 20;
-//var couponBookSold = prompt("How many Coupon Books did this student sell? ");
-//var couponBookTotal = couponBook * Number(couponBookSold);
+var couponBook = 20;
+var couponBookSold = prompt("How many Coupon Books did this student sell? ");
+var couponBookTotal = couponBook * Number(couponBookSold);
 var b = bakedPack(5,10);
 var bakedPackSold = prompt("How many Brownie Baskets did this student sell? ");
 var bakedPackTotal = b * Number(bakedPackSold);
-//var itemsSold = Number(kidsPackSold) + Number(couponBookSold) + Number(bakedPackSold);
+var itemsSold = Number(kidsPackSold) + Number(couponBookSold) + Number(bakedPackSold);
+var moneyRaised = kidsPackTotal + couponBookTotal + bakedPackTotal;
 
-console.log(bakedPackTotal);
+console.log(moneyRaised);
  
 
 
