9cd13350e027b05b2dc9437c372dbe72337b1f7e
joannamok <joannamok90@gmail.com>
2013-05-02 14:25:22 -0500

    added conditional ternary for hoiday promotion

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,6 +1,6 @@
 //Joanna Mokhtarezadeh  5-2-2013
 //Industry
-//Cost of Druzy Princess Cuff Design
+//Cost of Druzy Princess Cuff during Holiday Specails. Added bonus for customer purchases over $125.
 
 var crystals = prompt("How many crystals would you like to add to this design?");
 var pattWire = prompt("What is the cost of 3 inches of sterling pattern wire?");
@@ -9,15 +9,16 @@ var pattern = 1 * Number(pattWire);
 var crysCost = .08 * Number(crystals);
 var total = pharWrap + crysCost + pattern;
 var custCost = total * 2;
-
-
+var dicount = "Free Shipping";
+var freeGift = "Swarovski Earrings";
 
 function druzy(stone, silver){
 	var total = stone + silver;
 	return total;
 }
 
-console.log(custCost);
+freeGift = (custCost >= 125) ? "Your total material cost for this design is"+" "+"$"+total+". You should also give"+" "+freeGift+" "+"during Black Friday with this purchase." : "Your total material cost for this design is"+" "+"$"+total+" "+" You should also give"+" "+dicount+" "+"during Black Friday with this purchase.";
+console.log(freeGift);
 
 
 
