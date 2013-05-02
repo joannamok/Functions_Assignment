450d9ede6ff86942d1bc794ac6d44b02e2ec1eac
joannamok <joannamok90@gmail.com>
2013-05-01 21:10:27 -0500

    added prompts

--- Personal/js/script.js
+++ Personal/js/script.js
@@ -1,7 +1,6 @@
-
-
-//var lunch calories = Number("How many calories did you have for lunch?");
-//var dinner calories = Number("How many calories did you have for dinner?");
+//Joanna Mokhtarezadeh  5-1-2013
+//Personal
+//How many calories do I eat in a day
 
 bcals = function(coffee,cearal){	//function for total calories for a cup of coffee and a bowl of mini wheats
 	var bfastTotal = coffee + cearal;
@@ -13,10 +12,13 @@ scals = function(apple,orange){
 	return snackTotal;
 }  
 
-
-
 var b = bcals(100, 270);
 
 var s = scals(55,65);
-//console.log(b);
-console.log(s);
+
+var lunchCals = prompt("How many calories did you have for lunch?");
+var dinnerCals = prompt("How many calories did you have for dinner?");
+var totalDay = Number(lunchCals) + Number(dinnerCals);
+
+console.log(totalDay);
+ 
