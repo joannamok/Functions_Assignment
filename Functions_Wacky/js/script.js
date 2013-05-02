5c0c11bbdb779037fda2a2960a62db6c1c01f67d
joannamok <joannamok90@gmail.com>
2013-05-02 16:53:04 -0500

    added functions and some variables to wacky

---
+++ Functions_Wacky/js/script.js
@@ -0,0 +1,26 @@
+//Joanna Mokhtarezadeh  5-2-2013
+//Wacky
+// School Fundraiser
+
+kidsPack= function (books,tote){
+	kpTotal = books + tote;
+	return = kpTotal;
+}
+
+bakedPack = function (basket,brownies){
+	bpTotal = basket + brownies;
+	return = bpTotal;
+}
+
+
+var kidsPack = (10,5);
+var kidsPackSold = prompt("How many Children's Book Totebags did this student sell? ")
+var couponBook = 20;
+var couponBookSold = prompt("How many Coupon Books did this student sell? ")
+var bakedPack = (5,10);
+var bakedPackSold = ("How many Brownie Baskets did this student sell? ")
+var itemsSold = prompt("How many items did this student sell?")
+
+
+
+
