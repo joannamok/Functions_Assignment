2d0a52987301ec7c11bd38ec4c76862c3414d397
joannamok <joannamok90@gmail.com>
2013-05-02 19:14:32 -0500

    added string to logical opperator showing how many items were sold and howmuch money was raised by student

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -28,7 +28,7 @@ var smallPrize = "Flash Light Keyhcain";
 
 
 if(itemsSold >= 10 || moneyRaised >= 150){
-	console.log(bigPrize);
+	console.log("This student wins the grand prize gift of a"+" "+bigPrize+" ."+"They sold a total of"+" "+itemsSold+" "+"items and raised a total of"+" "+"$"+moneyRaised+".");
 }else{
 	console.log(smallPrize);
 }
